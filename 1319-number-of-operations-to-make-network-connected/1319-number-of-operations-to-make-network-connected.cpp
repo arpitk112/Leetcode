c92db12f26ead785ebe9fc9d7e735870ec813141
Arpit Kumar <160776769+arpitk112@users.noreply.github.com>
2026-08-31 12:54:17 +0530

    Time: 4 ms (79.39%), Space: 42 MB (96.94%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,37 +1,56 @@
 class Solution {
 public:
-    void DFS(unordered_map<int,vector<int>> &adj, vector<bool> &visited, int u){
-        visited[u] = true;
+    vector<int>parent;
+    vector<int>rank;
 
-        for(auto &v : adj[u]){
-            if(visited[v] == false){
-                DFS(adj,visited,v);
-            }
+    int find(int x){
+        if(x == parent[x]){
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y){
+        int x_par = find(x);
+        int y_par = find(y);
+
+        if(x_par == y_par){
+            return;
+        }
+
+        if(rank[x_par] > rank[y_par]){
+            parent[y_par] = x_par;
+        }else if(rank[y_par] > rank[x_par]){
+            parent[x_par] = y_par;
+        }else{
+            parent[x_par] = y_par;
+            rank[y_par]++;
         }
     }
 
     int makeConnected(int n, vector<vector<int>>& connections) {
-        unordered_map<int,vector<int>> adj;
-        int con = connections.size();
-        if(con < n - 1) return -1;
-        int count = 0;
+        parent.resize(n);
+        rank.resize(n);
+        int conn = n;
+        if(connections.size() < n - 1) return -1;
 
-        for(int i = 0; i < con; i++){
-            int u = connections[i][0];
-            int v = connections[i][1]; 
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+            rank[i] = 0;
         }
 
-        vector<bool>visited(n,false);
+        for(int i = 0; i<connections.size(); i++){
+            int u = connections[i][0];
+            int v = connections[i][1];
 
-        for(int i = 0; i<n; i++){
-            if(visited[i] == false){
-                DFS(adj,visited,i);
-                count++;
+            int parent_u = find(u);
+            int parent_v = find(v);
+
+            if(parent_u != parent_v){
+                Union(parent_u,parent_v);
+                conn--;
             }
         }
-        return count - 1;
+        return conn - 1;
     }
 };
